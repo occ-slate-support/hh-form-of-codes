7387d54023928c09e20eceb06da3cac421c41e9a
TeAnKe <197538480+TeAnKe@users.noreply.github.com>
2026-08-05 14:57:30 -0700

    Feature: Column Separate Scrollling

--- code-snippets.js
+++ code-snippets.js
@@ -1,13 +1,10 @@
-
 function copyHidden(button) {
-    const code = button.nextElementSibling.innerText;
-    const buttonTempText = button.innerText;
-    console.log("Copying code:", code);
-    navigator.clipboard.writeText(code).then(() => {
-        button.innerText = "Copied!";
-        setTimeout(() => {
-            button.innerText = buttonTempText;
-        }, 2000);
-    });
+  const code = button.nextElementSibling.innerText;
+  const buttonTempText = button.innerText;
+  navigator.clipboard.writeText(code).then(() => {
+    button.innerText = "Copied!";
+    setTimeout(() => {
+      button.innerText = buttonTempText;
+    }, 2000);
+  });
 }
-
