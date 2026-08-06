5de3fe630f562b8a1945273dbf767237b47f3c83
TeAnKe <197538480+TeAnKe@users.noreply.github.com>
2026-08-06 12:36:58 -0700

    Feature: Added code snippet box and copy feature

--- code-snippets.js
+++ code-snippets.js
@@ -1,10 +1,26 @@
+let resetTimer = null;
+
 function copyHidden(button) {
-  const code = button.nextElementSibling.innerText;
-  const buttonTempText = button.innerText;
-  navigator.clipboard.writeText(code).then(() => {
+  clearTimeout(resetTimer);
+
+  if (button.dataset.originalText) {
+    button.innerText = button.dataset.originalText;
+  }
+  else {
+    button.dataset.originalText = button.innerText;
+  }
+  
+  const codeSnippet = button.nextElementSibling.innerText;
+  const buttonTempHeight = button.offsetHeight + "px";
+  button.style.height = "";
+  
+  navigator.clipboard.writeText(codeSnippet).then(() => {
     button.innerText = "Copied!";
-    setTimeout(() => {
-      button.innerText = buttonTempText;
+    button.style.height = buttonTempHeight;
+    resetTimer = setTimeout(() => {
+      button.innerText = button.dataset.originalText;
+      button.style.height = "";
+      delete button.dataset.originalText;
     }, 2000);
   });
 }
